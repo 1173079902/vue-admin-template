6b8f6d583f46582ec6368b6e78037c1589cf01fc
1173079902 <1173079902@qq.com>
2021-03-19 11:00:24 +0800

    07_封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,11 @@
 // import request from '@/utils/request'
 
 export function login(data) {
-
+  return Request({
+    url: '/sys/login',
+    method: 'post',
+    data
+  })
 }
 
 export function getInfo(token) {
