41fc515f114135b28180b65b3b50e3da0d4b4aab
1173079902 <1173079902@qq.com>
2021-03-19 11:47:10 +0800

    09_封装登录的Action

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,5 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
+import { login } from '@/api/user'
 // 状态
 // 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
 // Vuex的持久化 如何实现 ？ Vuex和前端缓存相结合
@@ -20,7 +21,19 @@ const mutations = {
   }
 }
 // 执行异步
-const actions = {}
+const actions = {
+  // 定义login action  也需要参数 调用action时 传递过来的参数
+  async login(context, data) {
+    const result = await login(data) // 实际上就是一个promise  result就是执行的结果
+    // axios默认给数据加了一层data
+    if (result.data.success) {
+      // 表示登录接口调用成功 也就是意味着你的用户名和密码是正确的
+      // 现在有用户token
+      // actions 修改state 必须通过mutations
+      context.commit('setToken', result.data.data)
+    }
+  }
+}
 export default {
   namespaced: true,
   state,
