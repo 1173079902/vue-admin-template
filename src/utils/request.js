e9f9e28defce23f6f0faa83c320e167c9250a01d
1173079902 <1173079902@qq.com>
2021-03-20 15:34:57 +0800

    23_客户端处理TOKEN超时

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,10 @@
 // 导出一个axios的实例  而且这个实例要有请求拦截器 响应拦截器
 import store from '@/store'
-import { config } from '@vue/test-utils'
+import router from '@/router'
 import axios from 'axios'
 import { Message } from 'element-ui'
+import { getTimeStamp } from '@/utils/auth'
+const TimeOut = 3600 // 定义超时时间
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
@@ -11,6 +13,15 @@ const service = axios.create({
 // 请求拦截器
 service.interceptors.request.use(config => {
   if (store.getters.token) {
+    // 只有在有token的情况下 才有必要去检查时间戳是否超时
+    if (IsCheckTimeOut()) {
+      // 如果它为true表示 过期了
+      // token没用了 因为超时了
+      store.dispatch('user/logout') // 登出操作
+      // 跳转到登录页
+      router.push('/login')
+      return Promise.reject(new Error('token超时了'))
+    }
     config.headers['Authorization'] = `Bearer ${store.getters.token}`
   }
   return config
@@ -34,4 +45,11 @@ service.interceptors.response.use(response => {
   Message.error(error.message) // 提示错误信息
   return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
 })
+// 是否超时
+// 超时逻辑  (当前时间  - 缓存中的时间) 是否大于 时间差
+function IsCheckTimeOut() {
+  var currentTime = Date.now()
+  var timeStamp = getTimeStamp()
+  return (currentTime - timeStamp) / 1000 > TimeOut
+}
 export default service // 导出axios实例
