0df084321f0dbbd080acb4adb5cef2e1e67557d0
1173079902 <1173079902@qq.com>
2021-03-25 14:33:29 +0800

    75_建立公共导入的页面路由

---
+++ src/views/import/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- 公共导入组件 -->
+  <upload-excel />
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
