19fd8ef384b7d05a31ea9973354ca03d0cff4700
1173079902 <1173079902@qq.com>
2021-03-25 15:31:08 +0800

    77_实现Excel导入

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -4,6 +4,7 @@
 </template>
 
 <script>
+import { importEmployee } from '@/api/employees'
 export default {
   name: '',
 
@@ -12,7 +13,25 @@ export default {
   },
 
   methods: {
-    success({ header, results }) {}
+    async success({ header, results }) {
+      const userRelations = {
+        '入职日期': 'timeOfEntry',
+        '手机号': 'mobile',
+        '姓名': 'username',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber'
+      }
+      const arr = []
+      results.forEach(item => {
+        const userInfo = {}
+        Object.keys(item).forEach(key => {
+          userInfo[userRelations[key]] = item[key]
+        })
+        arr.push(userInfo)
+      })
+      await importEmployee(arr)
+      this.$message.success('上传成功')
+    }
   }
 }
 </script>
