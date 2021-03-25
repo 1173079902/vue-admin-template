6e12d9916d5cc512f89999bc0641a03c57b51c08
1173079902 <1173079902@qq.com>
2021-03-25 15:51:13 +0800

    修复导入bug

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,47 +1,37 @@
 <template>
-  <!-- 公共导入组件 -->
   <upload-excel :on-success="success" />
 </template>
 
 <script>
 import { importEmployee } from '@/api/employees'
 export default {
-  name: '',
-
-  data() {
-    return {}
-  },
-
+  name: 'Import',
   methods: {
     async  success({ header, results }) {
-      if (this.type === 'user') {
-        const userRelations = {
-          '入职日期': 'timeOfEntry',
-          '手机号': 'mobile',
-          '姓名': 'username',
-          '转正日期': 'correctionTime',
-          '工号': 'workNumber'
-        }
-        const arr = []
-        // 遍历所有的数组
-        results.forEach(item => {
-        // 需要将每一个条数据里面的中文都换成英文
-          const userInfo = {}
-          Object.keys(item).forEach(key => {
-          // key是当前的中文名 找到对应的英文名
-            if (userRelations[key] === 'timeOfEntry' || userRelations[key] === 'correctionTime') {
-              userInfo[userRelations[key]] = new Date(this.formatDate(item[key], '/')) // 只有这样, 才能入库
-              return
-            }
-            userInfo[userRelations[key]] = item[key]
-          })
-          // 最终userInfo变成了全是英文
-          arr.push(userInfo)
-        })
-        await importEmployee(arr)
-        this.$message.success('导入成功')
+      // 如果是导入员工
+      const userRelations = {
+        '入职日期': 'timeOfEntry',
+        '手机号': 'mobile',
+        '姓名': 'username',
+        '转正日期': 'correctionTime',
+        '工号': 'workNumber'
       }
-      this.$router.back() // 回到上一页
+      const arr = []
+      results.forEach(item => {
+        const userInfo = {}
+        Object.keys(item).forEach(key => {
+          // key 是当前的中文名，找到对应的英文名
+          if (userRelations[key] === 'timeOfEntry' || userRelations[key] === 'correctionTime') {
+            userInfo[userRelations[key]] = new Date(this.formatDate(item[key], '/'))
+            return
+          }
+          userInfo[userRelations[key]] = item[key]
+        })
+        arr.push(userInfo)
+      })
+      await importEmployee(arr)
+      this.$message.success('导入成功')
+      this.$router.back()
     },
     formatDate(numb, format) {
       const time = new Date((numb - 1) * 24 * 3600000 + 1)
@@ -58,4 +48,5 @@ export default {
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang='less' scoped>
+</style>
