bd2692bff572a4253ffb3f1c66c3585639354f1b
1173079902 <1173079902@qq.com>
2021-03-27 14:48:32 +0800

    90_新建文件上传组件

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-upload list-type="picture-card">
+    <i class="el-icon-plus" />
+  </el-upload>
+</template>
+
+<script>
+export default {
+  name: '',
+
+  data() {
+    return {}
+  },
+
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
