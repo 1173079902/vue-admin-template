27d71cb1d01d1169c63ce9d6faaf9326227bdaf5
1173079902 <1173079902@qq.com>
2021-03-27 15:54:10 +0800

    93_删除和添加图片

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -5,6 +5,8 @@
       :limit="1"
       action="#"
       :on-preview="preview"
+      :on-remove="handleRemove"
+      :on-change="changeFile"
       :file-list="fileList"
       :class="{ disabled: fileComputed }"
     >
@@ -44,6 +46,17 @@ export default {
       // 这里应该弹出一个层 层里是点击的图片地址
       this.imgUrl = file.url
       this.showDialog = true
+    },
+    handleRemove(file) {
+      // file是点击删除的文件
+      this.fileList = this.fileList.filter(item => item.uid !== file.uid)
+    },
+    changeFile(file, fileList) {
+      // 会执行多次
+      this.fileList = fileList.map(item => item)
+      // 上面之所以出不来，原因是上面的操作方式 this.fileList 最终还是为空数组，可如下写法
+      // fileList.length && (this.fileList = fileList.map(item => ({ url: item.url })))
+      // 上传成功 -> 数据才能进来 -> 腾讯云 OS
     }
   }
 }
