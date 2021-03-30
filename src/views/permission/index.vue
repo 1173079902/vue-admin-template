e3b44caf9859e9899fe369c666190d66abbfc05c
1173079902 <1173079902@qq.com>
2021-03-30 10:42:25 +0800

    109_准备编辑权限的弹层

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -21,6 +21,34 @@
         </el-table-column>
       </el-table>
     </div>
+    <!-- 放置一个弹层 用来编辑新增节点 -->
+    <el-dialog :title="`权限点`" :visible="showDialog" @close="btnCancel">
+      <!-- 表单 -->
+      <el-form ref="perForm" :model="formData" :rules="rules" label-width="120px">
+        <el-form-item label="权限名称" prop="name">
+          <el-input v-model="formData.name" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="权限标识" prop="code">
+          <el-input v-model="formData.code" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="权限描述">
+          <el-input v-model="formData.description" style="width:90%" />
+        </el-form-item>
+        <el-form-item label="开启">
+          <el-switch
+            v-model="formData.enVisible"
+            active-value="1"
+            inactive-value="0"
+          />
+        </el-form-item>
+      </el-form>
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
@@ -30,7 +58,20 @@ import { tranListToTreeData } from '@/utils'
 export default {
   data() {
     return {
-      list: []
+      list: [],
+      showDialog: true,
+      formData: {
+        name: '', // 名称
+        code: '', // 标识
+        description: '', // 描述
+        type: '', // 类型 该类型 不需要显示 因为点击添加的时候已经知道类型了
+        pid: '', // 因为做的是树 需要知道添加到哪个节点下了
+        enVisible: '0' // 开启
+      },
+      rules: {
+        name: [{ required: true, message: '权限名称不能为空', trigger: 'blur' }],
+        code: [{ required: true, message: '权限标识不能为空', trigger: 'blur' }]
+      }
     }
   },
   created() {
@@ -39,7 +80,9 @@ export default {
   methods: {
     async getPermissionList() {
       this.list = tranListToTreeData(await getPermissionList(), '0')
-    }
+    },
+    btnOK() {},
+    btnCancel() {}
   }
 }
 </script>
