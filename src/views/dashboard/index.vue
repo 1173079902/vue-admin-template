13e2bd8c236bbac811d0e9d794c8dde64b7b8ae1
1173079902 <1173079902@qq.com>
2021-04-01 15:22:52 +0800

    123_封装雷达图插件

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -85,7 +85,8 @@
           <div slot="header" class="header">
             <span>绩效指数</span>
           </div>
-        <!-- 放置雷达图 -->
+          <!-- 放置雷达图 -->
+          <radar />
         </el-card>
         <!-- 帮助连接 -->
         <el-card class="box-card">
@@ -124,11 +125,12 @@
 import { mapGetters, createNamespacedHelpers } from 'vuex'
 const { mapState } = createNamespacedHelpers('user')
 import WorkCalendar from './components/work-calendar'
+import Radar from './components/radar'
 
 export default {
   name: 'Dashboard',
   components: {
-    WorkCalendar
+    WorkCalendar, Radar
   },
   data() {
     return {
