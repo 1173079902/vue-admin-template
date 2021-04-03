22b71903976ffe956ed02a9bce3847fe1ca5126a
1173079902 <1173079902@qq.com>
2021-04-03 14:14:26 +0800

    133_Tab页的视图引入

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,7 @@ import ImageUpload from './ImageUpload'
 import ScreenFull from './ScreenFull'
 import ThemePicker from './ThemePicker'
 import Lang from './lang'
+import TagsView from './TagsView'
 export default {
   install(Vue) {
     //  注册全局的通用栏组件对象
@@ -14,5 +15,6 @@ export default {
     Vue.component('ScreenFull', ScreenFull) // 注册全屏组件
     Vue.component('ThemePicker', ThemePicker) // 注册主题组件
     Vue.component('Lang', Lang) // 注册切换多语言组件
+    Vue.component('TagsView', TagsView)// 注册多标签页组件
   }
 }
